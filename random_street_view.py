e71f7368bc9cca0aea81fcc3018b8d6aeb7d04c7
Hugo <hugovk@users.noreply.github.com>
2019-04-15 17:03:30 +0300

    Format with Black

--- random_street_view.py
+++ random_street_view.py
@@ -11,7 +11,8 @@ import getcolor
 # Optional, http://stackoverflow.com/a/1557906/724176
 try:
     import timing
-    assert(timing)  # avoid flake8 warning
+
+    assert timing  # avoid flake8 warning
 except ImportError:
     pass
 
@@ -19,26 +20,32 @@ except ImportError:
 # 25,000 image requests per 24 hours
 # See https://developers.google.com/maps/documentation/streetview/
 API_KEY = "INSERT_YOUR_API_KEY_HERE"
-GOOGLE_URL = ("http://maps.googleapis.com/maps/api/streetview?sensor=false&"
-              "size=640x640&key=" + API_KEY)
+GOOGLE_URL = (
+    "http://maps.googleapis.com/maps/api/streetview?sensor=false&"
+    "size=640x640&key=" + API_KEY
+)
 
 IMG_PREFIX = "img_"
 IMG_SUFFIX = ".jpg"
 
 parser = argparse.ArgumentParser(
     description="Get random Street View images from a given country",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 parser.add_argument(
-    '-n', '--images-wanted', type=int,
-    default=10,
-    help="Number of images wanted")
-parser.add_argument('country',  help='ISO 3166-1 Alpha-3 Country Code')
+    "-n", "--images-wanted", type=int, default=10, help="Number of images wanted"
+)
+parser.add_argument("country", help="ISO 3166-1 Alpha-3 Country Code")
 parser.add_argument(
-    '-hdg', '--heading',
-    help="Heading in degrees: 0 and 360 north, 90 east, 180 south, 270 west")
+    "-hdg",
+    "--heading",
+    help="Heading in degrees: 0 and 360 north, 90 east, 180 south, 270 west",
+)
 parser.add_argument(
-    '-p', '--pitch',
-    help="Pitch in degrees: 0 is default, 90 straight up, -90 straight down")
+    "-p",
+    "--pitch",
+    help="Pitch in degrees: 0 is default, 90 straight up, -90 straight down",
+)
 args = parser.parse_args()
 
 
@@ -49,13 +56,13 @@ def point_inside_polygon(x, y, poly):
     n = len(poly)
     inside = False
     p1x, p1y = poly[0]
-    for i in range(n+1):
+    for i in range(n + 1):
         p2x, p2y = poly[i % n]
         if y > min(p1y, p2y):
             if y <= max(p1y, p2y):
                 if x <= max(p1x, p2x):
                     if p1y != p2y:
-                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                        xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
                     if p1x == p2x or x <= xinters:
                         inside = not inside
         p1x, p1y = p2x, p2y
@@ -65,9 +72,10 @@ def point_inside_polygon(x, y, poly):
 print("Loading borders")
 shape_file = "TM_WORLD_BORDERS-0.3.shp"
 if not os.path.exists(shape_file):
-    print("Cannot find " + shape_file + ". Please download it from "
-          "http://thematicmapping.org/downloads/world_borders.php "
-          "and try again.")
+    print(
+        "Cannot find " + shape_file + ". Please download it from "
+        "http://thematicmapping.org/downloads/world_borders.php and try again."
+    )
     sys.exit()
 
 sf = shapefile.Reader(shape_file)
@@ -93,7 +101,7 @@ if not os.path.exists(args.country):
     os.makedirs(args.country)
 
 try:
-    while(True):
+    while True:
         attempts += 1
         rand_lat = random.uniform(min_lat, max_lat)
         rand_lon = random.uniform(min_lon, max_lon)
@@ -103,8 +111,7 @@ try:
             print("  In country")
             country_hits += 1
             lat_lon = str(rand_lat) + "," + str(rand_lon)
-            outfile = os.path.join(
-                args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
+            outfile = os.path.join(args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
             url = GOOGLE_URL + "&location=" + lat_lon
             if args.heading:
                 url += "&heading=" + args.heading
@@ -119,7 +126,7 @@ try:
                 # get_color returns the main color of image
                 color = getcolor.get_color(outfile)
                 print(color)
-                if color[0] == '#e3e2dd' or color[0] == "#e3e2de":
+                if color[0] == "#e3e2dd" or color[0] == "#e3e2de":
                     print("    No imagery")
                     imagery_misses += 1
                     os.remove(outfile)
