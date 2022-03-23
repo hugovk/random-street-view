a24e3d713d9528afe9debf7b869169e6c6056a47
Daniel Apushkinsky <createsdaniel@gmail.com>
2022-03-22 23:08:32 -0400

    Use metadata api instead of checking image color
    Instead of getting a streetview image, then checking if that image
    exists by looking at the color of it, we can use Google's metadata api
    to check if some location has streetview before getting the streetview
    image from that location. Unlike streetview image calls, these metadata
    calls are free (for now, verify below):
    https://developers.google.com/maps/documentation/streetview/metadata

--- random_street_view.py
+++ random_street_view.py
@@ -2,11 +2,10 @@ import argparse
 import os
 import random
 import sys
-from urllib.request import urlretrieve
+import json
+from urllib.request import urlretrieve, urlopen
 
 import shapefile  # pip install pyshp
-
-import getcolor
 
 # Optional, http://stackoverflow.com/a/1557906/724176
 try:
@@ -20,9 +19,12 @@ except ImportError:
 # 25,000 image requests per 24 hours
 # See https://developers.google.com/maps/documentation/streetview/
 API_KEY = "INSERT_YOUR_API_KEY_HERE"
-GOOGLE_URL = (
+GOOGLE_STREETVIEW_URL = (
     "http://maps.googleapis.com/maps/api/streetview?sensor=false&"
     "size=640x640&key=" + API_KEY
+)
+GOOGLE_METADATA_URL = (
+    "https://maps.googleapis.com/maps/api/streetview/metadata?key=" + API_KEY
 )
 
 IMG_PREFIX = "img_"
@@ -105,36 +107,43 @@ try:
         attempts += 1
         rand_lat = random.uniform(min_lat, max_lat)
         rand_lon = random.uniform(min_lon, max_lon)
-        # print attempts, rand_lat, rand_lon
         # Is (lat,lon) inside borders?
         if point_inside_polygon(rand_lon, rand_lat, borders):
             print("  In country")
             country_hits += 1
             lat_lon = str(rand_lat) + "," + str(rand_lon)
             outfile = os.path.join(args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
-            url = GOOGLE_URL + "&location=" + lat_lon
+            streetview_url = GOOGLE_STREETVIEW_URL + "&location=" + lat_lon
+            metadata_url = GOOGLE_METADATA_URL + "&location=" + lat_lon
+
+            with urlopen(metadata_url) as response:
+                body = response.read()
+
+            metadata_status = json.loads(body)["status"]
+            if metadata_status == "ZERO_RESULTS" or metadata_status == "NOT_FOUND":
+                imagery_misses += 1
+                print(f"  Image not found: {metadata_status}")
+                continue
+            elif metadata_status != "OK":
+                raise RuntimeError(
+                    f"Unexpected response from metadata query: {metadata_status}"
+                )
+
             if args.heading:
-                url += "&heading=" + args.heading
+                streetview_url += "&heading=" + args.heading
             if args.pitch:
-                url += "&pitch=" + args.pitch
+                streetview_url += "&pitch=" + args.pitch
             try:
-                urlretrieve(url, outfile)
+                urlretrieve(streetview_url, outfile)
             except KeyboardInterrupt:
                 sys.exit("exit")
+
             if os.path.isfile(outfile):
                 print(lat_lon)
-                # get_color returns the main color of image
-                color = getcolor.get_color(outfile)
-                print(color)
-                if color[0] == "#e3e2dd" or color[0] == "#e3e2de":
-                    print("    No imagery")
-                    imagery_misses += 1
-                    os.remove(outfile)
-                else:
-                    print("    ========== Got one! ==========")
-                    imagery_hits += 1
-                    if imagery_hits == args.images_wanted:
-                        break
+                print("    ========== Got one! ==========")
+                imagery_hits += 1
+                if imagery_hits == args.images_wanted:
+                    break
             if country_hits == MAX_URLS:
                 break
 except KeyboardInterrupt:
